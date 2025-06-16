ca5140068b4f527e54ca98229986f462e9f44b21
MunibaYasmeen <munibayasmeen1994@gmail.com>
2025-06-16 14:04:25 +0500

    requirements for running

--- Audio_Speech_Recognition/audio_processor.py
+++ Audio_Speech_Recognition/audio_processor.py
@@ -1,30 +1,16 @@
-import subprocess
 import librosa
-import ffmpeg
- 
+from moviepy.editor import VideoFileClip
+
 class AudioProcessor:
     def __init__(self):
         pass
 
-    def convert_video_to_audio(self, input_file, output_file):
-        ffmpeg_cmd = {
-            "ffmpeg",
-            "-i", input_file,
-            "-vn",
-            "-acodec","libmp3lame",
-            "-ab", "192k",
-            "-ar", "44100",
-            "-y",
-            output_file
-        }
+    def extract_mfcc_from_video(self, video_path, sr=16000, n_mfcc=13):
+        
+        clip = VideoFileClip(video_path)
+        audio_path = video_path.replace(".mp4", ".wav")
+        clip.audio.write_audiofile(audio_path, verbose=False, logger=None)
 
-        try:
-            subprocess.run(ffmpeg_cmd, check = True)
-            print("Successfully Converted!")
-        except subprocess.CalledProcessError as e:
-            print("Conversion Failed!")
-
-    def extract_mfcc_from_video(self, audio_file , sr=16000, n_mfcc=13):
-        y, sr = librosa.load(audio_file, sr=sr) 
+        y, sr = librosa.load(audio_path, sr=sr)
         mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
         return mfcc.mean(axis=1).tolist()  # reduce temporal dim
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Multimodal_SR_for_DS
+
+# pip install moviepy==1.0.3
+# pip install librosa
+# pip install pymongo
